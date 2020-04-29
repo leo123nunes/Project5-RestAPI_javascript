5d826339ac8f2f6666f4834e95c4a7a94eea4243
leo123nunes <ti.userleo@hotmail.com>
2020-04-29 13:04:37 -0300

    Installing express, nodemon , dependencies and sending data to the port 3032

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const port = 3003
+
+const express = require('express')
+
+const app = express()
+
+app.get('/products', (req,resp,next) => {
+    resp.send({name: "Book", price: 60, title: "The History of the World"})
+})
+
+app.listen(port,() => {
+    console.log(`Server is running at port ${port}...`)
+})
