144db7414a00f7a092891318d53c9723e7907467
leo123nunes <ti.userleo@hotmail.com>
2020-04-30 10:07:11 -0300

    Getting and saving products in database

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
 const port = 3003
 
+const database = require('./database')
+
 const express = require('express')
 
 const app = express()
 
 app.get('/products', (req,resp,next) => {
-    resp.send({name: "Book", price: 60, title: "The History of the World"})
+    resp.send(database.getProducts())
+})
+
+app.get('/products/:id', (req,resp,next)=>{
+    resp.send(database.getProduct(req.param.id))
+})
+
+app.post('/products',(req,resp,next)=>{
+    const product = database.saveProduct({
+        type: req.body.type,
+        price: req.body.price,
+        name: req.body.name
+    })
+    resp.psend(product)
 })
 
 app.listen(port,() => {
